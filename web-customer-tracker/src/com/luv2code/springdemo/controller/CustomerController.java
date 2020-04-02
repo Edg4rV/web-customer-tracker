5cae208c3169d0dec5c0b8623d50efaf2ae73076
EDGAR <vardedgar@mail.ru>
2020-04-02 20:59:34 +0400

    Simple CRUD project

--- web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.luv2code.springdemo.dao.CustomerDAO;
 import com.luv2code.springdemo.entity.Customer;
@@ -42,6 +43,26 @@ public class CustomerController {
 	    @PostMapping("/saveCustomer")
 	    public String saveCustomer(@ModelAttribute("customer") Customer theCustomer) {
 	    	customerService.saveCustomer(theCustomer);
+	    	return "redirect:/customer/list";
+	    }
+	    
+	    @GetMapping("/showFormForUpdate")
+	    public String showFormForUpdate(@RequestParam("customerId")int theId,
+	    								Model theModel) {
+	    	Customer theCustomer = customerService.getCustomer(theId);
+	    	
+	    	theModel.addAttribute("customer",theCustomer);
+	    	
+	    	return "customer-form";
+	    	
+	    }
+	    
+	    @GetMapping("/delete")
+	    public String deleteCustomer(@RequestParam("customerId") int theId) {
+	    	
+	    	customerService.deleteCustomer(theId);
+	    	
+	    	
 	    	return "redirect:/customer/list";
 	    }
 
